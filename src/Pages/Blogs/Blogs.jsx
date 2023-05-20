7335f5bb0bc382b6ad25bf9c669fdfaaa812f7d5
Alvihub <jonaiedhasan120@gmail.com>
2023-05-21 03:06:10 +0600

    Set the blog components

--- src/Pages/Blogs/Blogs.jsx
+++ src/Pages/Blogs/Blogs.jsx
@@ -3,8 +3,54 @@ import React from 'react';
 
 const Blogs = () => {
     return (
-        <div>
-            <h1>this is blog</h1>
+        <div className='my-container'>
+
+
+            <div className="App">
+                    <div className="collapse rounded-md my-4">
+                        <input type="checkbox" className="peer" />
+                        <div className="collapse-title bg-orange-400 text-white peer-checked:bg-secondary peer-checked:text-secondary-content">
+                            1.
+                        </div>
+                        <div className="collapse-content bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+                            <p>
+                               </p>
+                        </div>
+                    </div>
+                    <div className="collapse rounded-md my-4">
+                        <input type="checkbox" className="peer" />
+                        <div className="collapse-title bg-orange-400 text-white peer-checked:bg-secondary peer-checked:text-secondary-content">
+                            2.
+                        </div>
+                        <div className="collapse-content bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+                            <p></p>
+                        </div>
+                    </div>
+                    <div className="collapse rounded-md my-4">
+                        <input type="checkbox" className="peer" />
+                        <div className="collapse-title bg-orange-400 text-white peer-checked:bg-secondary peer-checked:text-secondary-content">
+                            3.
+                        </div>
+                        <div className="collapse-content bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+                            <p>.</p>
+                        </div>
+                    </div>
+                    <div className="collapse rounded-md my-4">
+                        <input type="checkbox" className="peer" />
+                        <div className="collapse-title bg-orange-400 text-white peer-checked:bg-secondary peer-checked:text-secondary-content">
+                            4.
+                        </div>
+                        <div className="collapse-content bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+                            <p></p>
+                        </div>
+                    </div>
+                </div>
+              
+           
+
+
+
+
         </div>
     );
 };
